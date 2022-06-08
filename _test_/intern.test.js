2128b14dd73613df9bfa2104ee245cd0c3dd9885
lyf703331869 <yliu52@nyit.edu>
2022-06-07 23:08:15 -0400

    Add intern test

---
+++ _test_/intern.test.js
@@ -0,0 +1,18 @@
+const Intern = require("../lib/intern");
+
+describe("Intern", () => {
+  it("should set a school value when passed in as a constructor argument", () => {
+    const school = "Columbia";
+    const int = new Intern("Yafei", "123", "lyf703331869@gmail.com", school);
+    expect(int.school).toEqual(school);
+  });
+
+  describe("getSchool", () => {
+    it("should retrieve the school username", () => {
+      const school = "Columbia";
+      const int = new Intern("Yafei", "123", "lyf703331869@gmail.com", school);
+
+      expect(int.getSchool()).toEqual(school);
+    });
+  });
+});
